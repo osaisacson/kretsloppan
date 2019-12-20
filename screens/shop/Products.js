088ec150a1d4c4fdcbbc13df2179dc2c882a1908
Osa Isacson <asaisacson@gmail.com>
2019-12-20 18:05:34 +0100

    add needed packages, set up dummy data

---
+++ screens/shop/Products.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Flatlist } from 'react-native';
+
+const Products = props => {
+  return <Flatlist />;
+};
+
+export default Products;
---
+++ store/reducers/products.js
@@ -0,0 +1,8 @@
+const initialState = {
+  availableProducts: [],
+  userProducts: []
+};
+
+export default (state = initialState, action) => {
+  return state;
+};
