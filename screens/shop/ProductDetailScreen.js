c37ca8e5cd24bee158d6278b85cc88695455d7ad
Osa Isacson <asaisacson@gmail.com>
2020-01-04 17:12:51 +0100

    styles for product detail

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -18,19 +18,44 @@ const ProductDetailScreen = props => {
   ); //gets a slice of the current state from combined reducers, then checks that slice for the item that has a matching id to the one we extract from the navigation above
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to cart" />
+      </View>
+      <Text style={styles.price}>{selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-//Overrides the default navigation options in the ShopNavigator
+//Sets/overrides the default navigation options in the ShopNavigator
 ProductDetailScreen.navigationOptions = navData => {
   return {
     headerTitle: navData.navigation.getParam('productTitle')
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: '100%'
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20
+  }
+});
 
 export default ProductDetailScreen;
