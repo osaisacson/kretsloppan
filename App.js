30e2d610ba1cd2676f2fba83fed03562555d06c4
Osa Isacson <asaisacson@gmail.com>
2020-01-04 17:34:48 +0100

    add custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font'; //Lets us use expo fonts
 
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
@@ -10,9 +12,31 @@ const rootReducer = combineReducers({
   products: productsReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer); //Redux, manages our state
+
+//Sets up requiring and asynchronically fetching our fonts when the app loads
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  //If font is not loaded yet (fontLoaded is false) return the AppLoading component which pauses the showing of the app until x has been met
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      ></AppLoading>
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -18,6 +18,12 @@ const ProductsNavigator = createStackNavigator(
       headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
       },
+      headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+      },
+      headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+      },
       headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary //headerTintColor === 'color'
     }
   }
