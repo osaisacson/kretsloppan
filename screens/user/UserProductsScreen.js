7204796ffefc5e33dadb020f336d83b16528fc64
Osa Isacson <asaisacson@gmail.com>
2020-01-07 17:47:49 +0100

    warningmodal when trying to delete an item

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
+import { FlatList, Button, Platform, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -11,6 +11,24 @@ import * as productsActions from '../../store/actions/products';
 const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
   const dispatch = useDispatch();
+
+  //Show an alert when trying to delete a product
+  const deleteHandler = id => {
+    Alert.alert(
+      'Are you sure?',
+      "Do you really want to delete this item? There's no going back.",
+      [
+        { text: 'Nope', style: 'default' },
+        {
+          text: 'Yes',
+          style: 'destructive',
+          onPress: () => {
+            dispatch(productsActions.deleteProduct(id));
+          }
+        }
+      ]
+    );
+  };
 
   const editProductHandler = id => {
     props.navigation.navigate('EditProduct', { productId: id }); //Navigate to the edit screen and forward the product id
@@ -39,9 +57,7 @@ const UserProductsScreen = props => {
           <Button
             color={Colors.primary}
             title="Delete"
-            onPress={() => {
-              dispatch(productsActions.deleteProduct(itemData.item.id));
-            }}
+            onPress={deleteHandler.bind(this, itemData.item.id)}
           />
         </ProductItem>
       )}
