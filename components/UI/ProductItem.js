4f95f038f389b5207b58b066c20e067298bc427b
Osa Isacson <asaisacson@gmail.com>
2020-03-13 20:08:46 +0100

    make productItem a PureComponent

--- components/UI/ProductItem.js
+++ components/UI/ProductItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 //Components
 import {
   View,
@@ -15,90 +15,94 @@ import { Ionicons } from '@expo/vector-icons';
 import Colors from './../../constants/Colors';
 import Styles from '../../constants/Styles';
 
-const ProductItem = props => {
-  let TouchableCmp = TouchableOpacity; //By default sets the wrapping component to be TouchableOpacity
-  //If platform is android and the version is the one which supports the ripple effect
-  if (Platform.OS === 'android' && Platform.Version >= 21) {
-    TouchableCmp = TouchableNativeFeedback;
-    //Set TouchableCmp to instead be TouchableNativeFeedback
-  }
+export default class ProductItem extends PureComponent {
+  render() {
+    let TouchableCmp = TouchableOpacity; //By default sets the wrapping component to be TouchableOpacity
+    //If platform is android and the version is the one which supports the ripple effect
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+      TouchableCmp = TouchableNativeFeedback;
+      //Set TouchableCmp to instead be TouchableNativeFeedback
+    }
 
-  return (
-    //TouchableOpacity lets us press the whole item to trigger an action. The buttons still work independently.
-    //'useForeground' has no effect on iOS but on Android it lets the ripple effect on touch spread throughout the whole element instead of just part of it
-    <View style={styles.container}>
-      <Card
-        style={props.isHorizontal ? styles.horizontalProduct : styles.product}
-      >
-        {props.itemData.status === 'bearbetas' ? (
-          <Ionicons
-            style={{
-              ...styles.icon,
-              backgroundColor: Colors.primary,
-              color: '#fff',
-              fontSize: 20,
-              paddingLeft: 8,
-              paddingRight: 8
-            }}
-            name={Platform.OS === 'android' ? 'md-hammer' : 'ios-hammer'}
-            size={23}
-            color={props.itemData.color}
-          />
-        ) : null}
-        {props.itemData.status === 'reserverad' ? (
-          <Ionicons
-            style={{
-              ...styles.icon,
-              color: Colors.primary,
-              fontSize: 35,
-              marginLeft: 3,
-              marginTop: -10
-            }}
-            name={Platform.OS === 'android' ? 'md-bookmark' : 'ios-bookmark'}
-            size={23}
-            color={props.itemData.color}
-          />
-        ) : null}
-        {props.itemData.status === 'hämtad' ? (
-          <Ionicons
-            style={{
-              ...styles.icon,
-              backgroundColor: Colors.completed,
-              color: '#fff',
-              paddingLeft: 10,
-              paddingRight: 10,
-              paddingBottom: 0,
+    return (
+      //TouchableOpacity lets us press the whole item to trigger an action. The buttons still work independently.
+      //'useForeground' has no effect on iOS but on Android it lets the ripple effect on touch spread throughout the whole element instead of just part of it
+      <View style={styles.container}>
+        <Card
+          style={props.isHorizontal ? styles.horizontalProduct : styles.product}
+        >
+          {props.itemData.status === 'bearbetas' ? (
+            <Ionicons
+              style={{
+                ...styles.icon,
+                backgroundColor: Colors.primary,
+                color: '#fff',
+                fontSize: 20,
+                paddingLeft: 8,
+                paddingRight: 8
+              }}
+              name={Platform.OS === 'android' ? 'md-hammer' : 'ios-hammer'}
+              size={23}
+              color={props.itemData.color}
+            />
+          ) : null}
+          {props.itemData.status === 'reserverad' ? (
+            <Ionicons
+              style={{
+                ...styles.icon,
+                color: Colors.primary,
+                fontSize: 35,
+                marginLeft: 3,
+                marginTop: -10
+              }}
+              name={Platform.OS === 'android' ? 'md-bookmark' : 'ios-bookmark'}
+              size={23}
+              color={props.itemData.color}
+            />
+          ) : null}
+          {props.itemData.status === 'hämtad' ? (
+            <Ionicons
+              style={{
+                ...styles.icon,
+                backgroundColor: Colors.completed,
+                color: '#fff',
+                paddingLeft: 10,
+                paddingRight: 10,
+                paddingBottom: 0,
 
-              fontSize: 25
-            }}
-            name={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
-            size={23}
-            color={props.itemData.color}
-          />
-        ) : null}
-        <View style={styles.touchable}>
-          <TouchableCmp onPress={props.onSelect} useForeground>
-            {/* This extra View is needed to make sure it fulfills the criteria of child nesting on Android */}
-            <View>
-              <View style={styles.imageContainer}>
-                <Image
-                  style={styles.image}
-                  source={{ uri: props.itemData.image }}
-                />
+                fontSize: 25
+              }}
+              name={
+                Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+              }
+              size={23}
+              color={props.itemData.color}
+            />
+          ) : null}
+          <View style={styles.touchable}>
+            <TouchableCmp onPress={props.onSelect} useForeground>
+              {/* This extra View is needed to make sure it fulfills the criteria of child nesting on Android */}
+              <View>
+                <View style={styles.imageContainer}>
+                  <Image
+                    style={styles.image}
+                    source={{ uri: props.itemData.image }}
+                  />
+                </View>
+                <Text style={styles.price}>
+                  {props.itemData.price ? props.itemData.price : 0} kr
+                </Text>
               </View>
-              <Text style={styles.price}>
-                {props.itemData.price ? props.itemData.price : 0} kr
-              </Text>
-            </View>
-          </TouchableCmp>
-        </View>
-      </Card>
-      <Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.title}>
-        {props.itemData.title}
-      </Text>
-    </View>
-  );
-};
+            </TouchableCmp>
+          </View>
+        </Card>
+        <Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.title}>
+          {props.itemData.title}
+        </Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
