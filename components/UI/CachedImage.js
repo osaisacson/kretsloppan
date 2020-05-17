d567cb66f474ef69d99fbcbbd8b598995e3a2bff
Osa Isacson <asaisacson@gmail.com>
2020-05-17 12:21:44 +0200

    test CachedImage

--- components/UI/CachedImage.js
+++ components/UI/CachedImage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Image, CacheManager } from "react-native-expo-image-cache";
-import * as ImageManipulator from "expo-image-manipulator";
+import { Image, CacheManager } from 'react-native-expo-image-cache';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 const CachedImage = (props) => {
   const [source, setSource] = useState(null);
@@ -11,23 +11,25 @@ const CachedImage = (props) => {
   useEffect(() => {
     let isSubscribed = true;
     base64Image()
-      .then((source) => (isSubscribed ? setSource(`data:image/jpeg;base64,${source.base64 || ""}`) : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='))
+      .then((source) =>
+        isSubscribed
+          ? setSource(`data:image/jpeg;base64,${source.base64 || ''}`)
+          : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='
+      )
       .catch((error) => (isSubscribed ? setError(error.toString()) : null));
-
     return () => (isSubscribed = false);
   }, []);
-
 
   const base64Image = useCallback(async () => {
     await CacheManager.get(uri).getPath();
     await ImageManipulator.manipulateAsync(
       uri,
       [{ resize: { width: 5, height: 5 } }],
-      { base64: true, format: "jpeg" }
+      { base64: true, format: 'jpeg' }
     );
   }, []);
 
-  return <Image style={props.style} {...{ preview: source, uri }} />
+  return <Image style={props.style} {...{ preview: source, uri }} />;
 };
 
 export default CachedImage;
