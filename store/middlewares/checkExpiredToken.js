cf2b1d8927eb64eec666507c21e8863ccceb042e
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-06-02 18:42:57 +0800

    Added checkExpiredToken middleware
    If the token is expired, the user will be logged out

---
+++ store/middlewares/checkExpiredToken.js
@@ -0,0 +1,23 @@
+import { AsyncStorage } from 'react-native';
+
+import { logout } from '../actions/auth';
+
+const checkExpiredToken = (store) => (next) => async (action) => {
+  const userData = await AsyncStorage.getItem('userData').then((data) =>
+    data ? JSON.parse(data) : {}
+  );
+
+  if (userData) {
+    const tokenExpiry = new Date(userData.expiryDate).getTime();
+    const now = new Date().getTime();
+
+    if (tokenExpiry <= now) {
+      logout(next);
+      return;
+    }
+  }
+
+  return next(action);
+};
+
+export default checkExpiredToken;
