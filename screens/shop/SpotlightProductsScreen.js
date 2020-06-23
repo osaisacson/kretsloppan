5cb404308f83e307f0abe17441b26209858db272
Osa Isacson <asaisacson@gmail.com>
2020-06-23 15:36:54 +0200

    show notifications on spotlightscreen as well

--- screens/shop/SpotlightProductsScreen.js
+++ screens/shop/SpotlightProductsScreen.js
@@ -1,17 +1,22 @@
 import { MaterialCommunityIcons, Entypo, FontAwesome5 } from '@expo/vector-icons';
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import ActionLine from '../../components/UI/ActionLine';
 import HorizontalScroll from '../../components/UI/HorizontalScroll';
 import Introduction from '../../components/UI/Introduction';
 import SaferArea from '../../components/UI/SaferArea';
+import UserActions from '../user/UserActions';
 
 const SpotlightProductsScreen = (props) => {
+  const [showUserActions, setShowUserActions] = useState(false);
+
   const allProducts = useSelector((state) => state.products.availableProducts);
   const allProjects = useSelector((state) => state.projects.availableProjects);
   const allProposals = useSelector((state) => state.proposals.availableProposals);
   const currentProfile = useSelector((state) => state.profiles.userProfile);
+  const loggedInUserId = useSelector((state) => state.auth.userId);
 
   //Filters all products with the tag 'redo'
   const recentProductsRaw = allProducts.filter(
@@ -39,6 +44,41 @@ const SpotlightProductsScreen = (props) => {
   const recentProjects = recentProjectsSorted.slice(0, 5);
   const recentProposals = recentProposalsSorted.slice(0, 5);
 
+  //Gets all products by the logged in user
+  const userProducts = useSelector((state) => state.products.userProducts);
+
+  //RESERVED: Gets all products reserved by the user or from the user
+  const reservedAllProductsRaw = allProducts.filter(
+    (product) => product.status === 'reserverad' && product.reservedUserId === loggedInUserId
+  );
+  const reservedByOthersRaw = userProducts.filter((product) => product.status === 'reserverad');
+  const reservedProductsRaw = reservedAllProductsRaw.concat(reservedByOthersRaw);
+  const reservedProducts = reservedProductsRaw.sort(function (a, b) {
+    a = new Date(a.reservedDate);
+    b = new Date(b.reservedDate);
+    return b > a ? -1 : b < a ? 1 : 0;
+  });
+
+  //TO BE COLLECTED FROM: Gets all products from the user marked as ready to be collected
+  const toBeSoldRaw = userProducts.filter((product) => product.status === 'ordnad');
+  const toBeSold = toBeSoldRaw.sort(function (a, b) {
+    a = new Date(a.collectingDate);
+    b = new Date(b.collectingDate);
+    return b > a ? -1 : b < a ? 1 : 0;
+  });
+
+  //TO BE COLLECTED BY: Gets all products marked as ready to be collected by the user
+  const toBeBoughtRaw = allProducts.filter(
+    (product) => product.status === 'ordnad' && product.collectingUserId === loggedInUserId
+  );
+  const toBeBought = toBeBoughtRaw.sort(function (a, b) {
+    a = new Date(a.collectingDate);
+    b = new Date(b.collectingDate);
+    return b > a ? -1 : b < a ? 1 : 0;
+  });
+
+  const badgeNr = reservedProducts.length + toBeBought.length + toBeSold.length;
+
   return (
     <SaferArea>
       {!currentProfile.hasReadNews ? (
@@ -49,7 +89,25 @@ const SpotlightProductsScreen = (props) => {
           text="NYHETER: Kretsloppan släppt, hurra! För feedback kontakta asaisacson@gmail.com, vi gör kontinuerliga uppdateringar. Version: 1.0-beta3"
         />
       ) : null}
+
       <ScrollView showsVerticalScrollIndicator={false} nestedScrollEnabled>
+        {badgeNr ? (
+          <ActionLine
+            isActive={showUserActions}
+            badgeNr={badgeNr}
+            onPress={() => {
+              setShowUserActions(!showUserActions);
+            }}
+          />
+        ) : null}
+        {showUserActions ? (
+          <UserActions
+            reservedProducts={reservedProducts}
+            toBeBought={toBeBought}
+            toBeSold={toBeSold}
+            navigation={props.navigation}
+          />
+        ) : null}
         <HorizontalScroll
           largeImageItem
           detailPath="ProjectDetail"
